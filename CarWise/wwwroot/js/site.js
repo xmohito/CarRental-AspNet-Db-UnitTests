b05d3e8a4e664a57b6f62a12aaaf0c6b7b65ce4a
x_mohito <brozda.it@gmail.com>
2023-02-01 23:21:39 +0100

    Added faq page section

--- CarWise/wwwroot/js/site.js
+++ CarWise/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
