ec52230b83c3e5c8bb6bc05716a8d40752266280
x_mohito <brozda.it@gmail.com>
2023-02-10 23:51:02 +0100

    Reorganized DbContext

---
+++ CarWise/Data/AppDbContext - Copy.cs
@@ -0,0 +1,13 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+//namespace CarWise.Data
+//{
+//    public class AppDbContext : DbContext
+//    {
+//        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+//        public DbSet<Models.User> Users { get; set; } = default!;
+
+//        public DbSet<Models.Car> Cars { get; set; } = default!;
+//    }
+//}
--- CarWise/Data/AppDbContext.cs
+++ CarWise/Data/AppDbContext.cs
@@ -1,13 +1,159 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using CarWise.Models;
+
+#nullable disable
 
 namespace CarWise.Data
 {
-    public class AppDbContext : DbContext
+    public partial class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Models.User> Users { get; set; } = default!;
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
-        public DbSet<Models.Car> Cars { get; set; } = default!;
+        public virtual DbSet<BodyType> BodyTypes { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Fuel> Fuels { get; set; }
+        public virtual DbSet<Rental> Rentals { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Admin\\Desktop\\Projekt\\CarWise\\CarWise\\CarWiseDB.mdf;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<BodyType>(entity =>
+            {
+                entity.ToTable("BodyType");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Brand)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IdBodyType).HasColumnName("ID_BodyType");
+
+                entity.Property(e => e.IdFuel).HasColumnName("ID_Fuel");
+
+                entity.Property(e => e.Model)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PriceForDay).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.IdBodyTypeNavigation)
+                    .WithMany(p => p.Cars)
+                    .HasForeignKey(d => d.IdBodyType)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Cars__ID_BodyTyp__5AEE82B9");
+
+                entity.HasOne(d => d.IdFuelNavigation)
+                    .WithMany(p => p.Cars)
+                    .HasForeignKey(d => d.IdFuel)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Cars__ID_Fuel__5BE2A6F2");
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Fuel>(entity =>
+            {
+                entity.ToTable("Fuel");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.TypeOfFuel)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Rental>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.IdCar).HasColumnName("ID_Car");
+
+                entity.Property(e => e.IdCustomer).HasColumnName("ID_Customer");
+
+                entity.Property(e => e.ReceiptDate).HasColumnType("date");
+
+                entity.Property(e => e.ReturnDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdCarNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdCar)
+                    .HasConstraintName("FK__Rentals__ID_Car__6E01572D");
+
+                entity.HasOne(d => d.IdCustomerNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdCustomer)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Rentals__ID_Cust__5CD6CB2B");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
