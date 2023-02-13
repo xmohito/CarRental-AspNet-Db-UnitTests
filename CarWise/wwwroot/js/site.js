e5d352d6a0cd9b11d4ab7bab44b841cb5d79baf5
x_mohito <brozda.it@gmail.com>
2023-02-13 19:55:33 +0100

    Changes at rental page, added form

--- CarWise/wwwroot/js/site.js
+++ CarWise/wwwroot/js/site.js
@@ -3,6 +3,7 @@
 
 // Write your JavaScript code.
 
+///faq js
 var faq = document.getElementsByClassName("faq-page");
 var i;
 for (i = 0; i < faq.length; i++) {
@@ -19,3 +20,28 @@ for (i = 0; i < faq.length; i++) {
         }
     });
 }
+//swiper js
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+document.querySelector("#rent-this-car").addEventListener("click", function () {
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+});
