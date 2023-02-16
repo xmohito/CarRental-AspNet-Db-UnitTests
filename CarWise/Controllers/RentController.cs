20b9c0f08a88cd3337ce8d9319112d55214c7004
x_mohito <brozda.it@gmail.com>
2023-02-16 20:57:36 +0100

    Added logic into customer form at Rent page

--- CarWise/Controllers/RentController.cs
+++ CarWise/Controllers/RentController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using CarWise.Controllers;
 using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
 namespace CarWise.Controllers
 {
@@ -20,55 +21,119 @@ namespace CarWise.Controllers
         {
             db = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string Message)
         {
+            ViewData["Message"] = Message;
             var carData = db.Cars.ToList();
             return View(carData);
 
         }
-        
+
+        public bool IsAtLeast18YearsOld(DateTime birthdate)
+        {
+            int age = DateTime.Today.Year - birthdate.Year;
+            if (birthdate > DateTime.Today.AddYears(-age))
+            {
+                age--;
+            }
+            return age >= 18;
+        }
+
+        public bool IsValidPhoneNumber(string phonenumberString)
+        {
+            int number;
+            bool isInteger = int.TryParse(phonenumberString, out number);
+
+            bool has9Digits = phonenumberString.Length == 9;
+
+            return isInteger && has9Digits;
+        }
+
+        public bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            bool isMatch = Regex.IsMatch(email, pattern);
+
+            return isMatch;
+        }
+
         [HttpPost]
         public IActionResult Index(string name, string surname, DateTime birthdate, int phonenumber, string email, DateTime receiptdate, DateTime returndate, int car)
         {
             using (var contex = db.Database.BeginTransaction())
             {
-                if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(surname) && phonenumber != 0 && !string.IsNullOrWhiteSpace(email))
+                if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(surname) && !string.IsNullOrWhiteSpace(email) && birthdate != DateTime.MinValue
+                    && receiptdate != DateTime.MinValue && returndate != DateTime.MinValue && car != 0)
                 {
-                    var insertedCustomer = new Customer
+                    string phonenumberString = phonenumber.ToString();
+                    if (IsValidPhoneNumber(phonenumberString))
                     {
-                        Name = name,
-                        Surname = surname,
-                        BirthDate = birthdate,
-                        PhoneNumber = phonenumber,
-                        Email = email
-                    };
-                    db.Customers.Add(insertedCustomer);
-                    db.SaveChanges();
-                    Console.WriteLine(car);
-                    var insertedRent = new Rental
-                    {
-                        IdCar = car,
-                        IdCustomer = insertedCustomer.Id,
-                        ReceiptDate = receiptdate,
-                        ReturnDate = receiptdate,
-                        ToPay = 1,
-                        Pay = false
+                        if (IsValidEmail(email)){
+                            if (IsAtLeast18YearsOld(birthdate))
+                            {
+                                if(receiptdate == DateTime.MinValue || returndate == DateTime.MinValue || receiptdate > returndate || receiptdate < DateTime.Today)
+                                {
+                                    return RedirectToAction("index", "Rent", new { Message = "Enter a valid booking date range" });
+                                }
+                                else
+                                {
+                                    var insertedCustomer = new Customer
+                                    {
+                                        Name = name,
+                                        Surname = surname,
+                                        BirthDate = birthdate,
+                                        PhoneNumber = phonenumber,
+                                        Email = email
+                                    };
+                                    db.Customers.Add(insertedCustomer);
+                                    db.SaveChanges();
+                                    Console.WriteLine(car);
+                                    var insertedRent = new Rental
+                                    {
+                                        IdCar = car,
+                                        IdCustomer = insertedCustomer.Id,
+                                        ReceiptDate = receiptdate,
+                                        ReturnDate = returndate,
+                                        ToPay = 1,
+                                        Pay = false
 
-                    };
-                    db.Rentals.Add(insertedRent);
-                    db.SaveChanges();
-                    contex.Commit();
-                    ViewData["Message"] = "Added";
-                    return RedirectToAction("index", "Rent");
+                                    };
+                                    db.Rentals.Add(insertedRent);
+                                    db.SaveChanges();
+                                    contex.Commit();
+                                    return RedirectToAction("index", "Rent");
+                                }
+                            }
+                            else
+                            {
+                                return RedirectToAction("index", "Rent", new { Message = "You must be 18 years old to rent a car" });
+                            }
+                        }
+                        else
+                        {
+                            return RedirectToAction("index", "Rent", new { Message = "Enter vaild email" });
+                        }
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("index", "Rent", new { Message = "Enter correct type of phone number" });
+                    }
+
+
+
+
+
                 }
                 else
                 {
-                    ViewData["Message"] = "Incorrect data!";
-                    return View();
+                    return RedirectToAction("index", "Rent", new {Message = "Enter all data" });
                 }
-
-
-                return View();
                 
 
             }
