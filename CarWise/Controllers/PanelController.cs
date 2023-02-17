920aa2e70e72189e7a73a985e432a1d322c3474f
x_mohito <brozda.it@gmail.com>
2023-02-18 00:00:09 +0100

    Added menagement options to PanelView of editing and deleting reservations

--- CarWise/Controllers/PanelController.cs
+++ CarWise/Controllers/PanelController.cs
@@ -1,4 +1,5 @@
 ﻿using CarWise.Data;
+using CarWise.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -15,10 +16,115 @@ namespace CarWise.Controllers
         {
             _context = context;
         }
+
+        // GET: Rentals
         public IActionResult Index()
         {
-            var instructors = _context.Customers.ToList();
-            return View(instructors);
+            var rentals = _context.Rentals.ToList();
+            return View(rentals);
+        }
+
+        // GET: Rentals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rentals/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdCustomer,IdCar,ReceiptDate,ReturnDate,ToPay,Pay")] Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rental);
+        }
+
+        // GET: Rentals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            return View(rental);
+        }
+
+        // POST: Rentals/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdCustomer,IdCar,ReceiptDate,ReturnDate,ToPay,Pay")] Rental rental)
+        {
+            if (id != rental.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rental);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentalExists(rental.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rental);
+        }
+
+        // GET: Rentals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // POST: Rentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rental = await _context.Rentals.FindAsync(id);
+            _context.Rentals.Remove(rental);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalExists(int id)
+        {
+            return _context.Rentals.Any(e => e.Id == id);
         }
     }
 }
